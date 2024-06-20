9d4e10b5659fecf204d257c32bb86285d8cf01ca
kkkisme <100huming@gmail.com>
2024-06-20 11:23:18 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,13 +6,18 @@ defineProps({
 })
 
 const count = ref(0)
+
+const handleClick = () => {
+  count.value = count.value +1
+  fetch('/test-api', () => {})
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="handleClick">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
